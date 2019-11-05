b0fcf1247345891c86f09cea52f889161867731e
Israel Oliveira <israel@radek.com.br>
2019-11-05 17:12:36 -0300

    Add test just for sample

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+const puppeteer = require('puppeteer');
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const isDebugging = () => {
+  const debugging_mode = {
+    headless: false,
+    slowMo: 2500,
+    devtools: true,
+    // executablePath: string,
+    // timeout: number,
+    // ignoreHTTPSErrors: bool
+  };
+  return process.env.NODE_ENV === 'debug' ? debugging_mode : {};
+};
+
+describe('on page load', () => {
+  test('link loads correctly', async () => {
+    let browser = await puppeteer.launch(isDebugging());
+    let page = await browser.newPage();
+
+    page.emulate({
+      viewport: {
+        width: 1366,
+        height: 2400
+      },
+      userAgent: ''
+    })
+
+    await page.goto('http://localhost:3000/');
+    //.$eval its like a document.querySelector
+    const html = await page.$eval('.App-link', e => e.innerHTML);
+
+    expect(html).toBe('Learn React');
+
+    await browser.close();
+  }, 1600)
 });
